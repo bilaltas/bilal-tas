5950cc45f7dc96193ff90a52ed71e57896d929fe
Bilal TAS <bilaltas@me.com>
2021-05-17 00:12:39 +0300

    Default Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 module.exports = {
-	purge: [],
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-		extend: {},
-	},
-	variants: {
-		extend: {},
-	},
+	theme: {},
+	variants: {},
 	plugins: [],
+	purge: {
+		content: [`components/**/*.{vue,js}`, `layouts/**/*.vue`, `pages/**/*.vue`, `plugins/**/*.{js,ts}`, `nuxt.config.{js,ts}`],
+	},
 }
