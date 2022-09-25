3631a7144daf5b12368d6ed7e12eafcfc8ba2f85
Bilal TAŞ <12401765+bilaltas@users.noreply.github.com>
2022-09-25 11:42:40 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="max-w-xl px-0 sm:px-12 lg:px-32">
 		<p class="font-sans text-xs text-gray-600 uppercase tracking-wide mb-1 sm:mb-3">Currently</p>
-		<h1 class="text-2xl md:text-4xl leading-tight lg:leading-normal mb-5 sm:mb-8 font-serif">Websites Technical Manager at <span class="inline-block" style="color: #6ce1ae">Invicti Security</span></h1>
+		<h1 class="text-2xl md:text-4xl leading-tight lg:leading-normal mb-5 sm:mb-8 font-serif">Websites Team Technical Manager at <span class="inline-block" style="color: #6ce1ae">Invicti Security</span></h1>
 
 		<div class="mx-0 sm:ml-12 sm:-mr-12">
 			<p class="pr-6 md:pr-0 text-xs leading-normal text-gray-900 mb-8">Hello, I’m Bilal, senior full-stack web developer. I create pixel-perfect web sites/apps. Started programming on 2006, working in the field professionally for about 10 years now. Currently living and working remotely in Turkey for 6 years.</p>
