037deb31372842291b6cee76ee92bead2556efd4
Bilal TAŞ <12401765+bilaltas@users.noreply.github.com>
2022-09-26 12:00:20 +0300

    Update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -4,8 +4,8 @@
 			<NuxtLink to="/" class="font-serif block text-black no-underline hover:text-gray-600">
 				<span v-if="$nuxt.$route.name == 'tr'" class="text-2xl">Bilal TAŞ</span>
 				<span v-else class="text-2xl">Bilal TAS</span> <br />
-				<small v-if="$nuxt.$route.name == 'tr'" class="relative top-0 sm:-top-1 text-gray-500 block leading-4">Wordpress Geliştiricisi, <span class="hidden sm:inline">Vue Sever,</span> <span class="inline-block">Takım Lideri</span></small>
-				<small v-else class="relative top-0 sm:-top-1 text-gray-500 block leading-4">Wordpress Developer, <span class="hidden sm:inline">Vue Lover,</span> <span class="inline-block">Team Leader</span></small>
+				<small v-if="$nuxt.$route.name == 'tr'" class="relative top-0 sm:-top-1 text-gray-500 block leading-4">Wordpress Geliştiricisi, <span class="hidden sm:inline">JS Sever,</span> <span class="inline-block">Takım Lideri</span></small>
+				<small v-else class="relative top-0 sm:-top-1 text-gray-500 block leading-4">Wordpress Developer, <span class="hidden sm:inline">JS Lover,</span> <span class="inline-block">Team Leader</span></small>
 			</NuxtLink>
 		</div>
 
