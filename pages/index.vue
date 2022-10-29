a72a1582d7305572967f434b94852bac29a6bb42
Bilal TAŞ <12401765+bilaltas@users.noreply.github.com>
2022-10-29 13:30:28 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
 		<h1 class="text-2xl md:text-4xl leading-tight lg:leading-normal mb-5 sm:mb-8 font-serif">Websites Team Technical Manager at <span class="inline-block" style="color: #6ce1ae">Invicti Security</span></h1>
 
 		<div class="mx-0 sm:ml-12 sm:-mr-12">
-			<p class="pr-6 md:pr-0 text-xs leading-normal text-gray-900 mb-8">Hello I’m Bilal, senior full-stack web developer who also has decent experience on engineering management. I've built and helped teams building pixel-perfect web sites/apps in reliable architecture. Started programming on 2006, working in the field professionally for about 10 years now. Over last 5 years, I've lead web development teams. Currently living and working remotely in Turkey for 7 years.</p>
+			<p class="pr-6 md:pr-0 text-xs leading-normal text-gray-900 mb-8">Hello I’m Bilal, engineering manager who has senior full-stack web development experiences. I’ve built and helped teams building pixel-perfect web sites/apps in reliable architecture. Started programming on 2006, working in the field professionally for about 10 years now. I’ve led web development teams over last 5 years. Currently living and working remotely in Turkey for 7 years.</p>
 			<Social />
 		</div>
 	</div>
